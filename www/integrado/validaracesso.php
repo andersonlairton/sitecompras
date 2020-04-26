d0479b417f29fe79a66ac65088b3868067c06405
Anderson Lairton <lairton.anderson@gmail.com>
2020-04-26 18:33:25 -0300

    configurando variaveis de sessão

--- www/integrado/validaracesso.php
+++ www/integrado/validaracesso.php
@@ -1,45 +1,29 @@
+<?php
 
-<html>
-<title></title>
-<head>
-   
-    <?php
-   include "config.php";//0606
-   $CNPJ=$_POST["CNPJ"]; 
-   $Senha=$_POST["Senha"]; 
-   
-    session_start();
-    //include "config.php";
-    
-    $SQL = sprintf("SELECT * FROM tb_forncedor_npj WHERE CNPJ = '$CNPJ' AND Senha = '$Senha'");
-    
-  
+    include "config.php";
+    $CNPJ=$_POST["CNPJ"]; 
+    $Senha=$_POST["Senha"]; 
+    $SQL = sprintf("SELECT * FROM tb_forncedor_npj WHERE CNPJ = '$CNPJ' AND SENHA = '$Senha'");
     
     $result = mysqli_query($conexao,$SQL) or die(mysqli_error());//comando sql
+
+    $dado = mysqli_fetch_assoc($result);
+   
     $row = mysqli_num_rows($result);//checando no banco se a informção existe
-    
-    if ($row> 0){ //condição,se tiver mais qe um registo ele acessa o banco
-      session_start();
+
+    if ($row== 1){ //condição,se tiver mais qe um registo ele acessa o banco
+        
+        session_start();
         $_SESSION['CNPJ']=$_POST['CNPJ'];
-        
+        $_SESSION['NOME']=$dado['NOME'];
+        $_SESSION['ENDERECO']=$dado['ENDERECO'];
+        $_SESSION['EMAIL']=$dado['EMAIL'];
+        $_SESSION['TELEFONE']=$dado['TELEFONE'];
+        $_SESSION['NOME_EMPRESA']=$dado['NOME_EMPRESA'];
         $_SESSION['Senha']=$_POST['Senha'];
-        
-        echo "<center>Login feito com sucesso $CNPJ";
-       // $_SESSION['mycnpj'] =$CNPJ['CNPJ'];
-       //  $_SESSION["mycnpj"] = $CNPJ["CNPJ"];
-        header("location:telafornecedor.php");
-        $SQL2 = sprintf("SELECT NOME FROM tb_cliente WHERE CNPJ = $CNPJ");
-        $_SESSION['MYNAME']=$SQL2["NOME"];
-        
+        header('Location:telafornecedor.php');
     }else{
         echo"<center>login invalido</center>";
         
     }
-    
-    
-    ?>
-    
-    </head>
-</html>
-    
-  
+?>
