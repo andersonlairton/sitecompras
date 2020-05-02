117f2fa29b896d92492e0cdc58abb326bc59c07c
Anderson Lairton <lairton.anderson@gmail.com>
2020-05-02 01:48:39 -0300

    configurando servidor db4free de banco de dados

--- www/integrado/config.php
+++ www/integrado/config.php
@@ -1,8 +1,8 @@
 <?php
  
-    $host = "bd";//"127.0.0.1";
-    $user = "root";
-    $pass = "password";
+    $host = "db4free.net";//"bd";//"127.0.0.1";//futuramente alterar para o 000webhost
+    $user = "site_compras";
+    $pass = "!tB4=R&DdA|]36%*";
     $banco="sitecompras";
 
     $conexao = mysqli_connect($host, $user, $pass) or die(mysqli_error);
