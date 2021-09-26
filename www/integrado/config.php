02b461e4e094b5fd83edfb0c9f3d98d7947ff41d
andersonlairton <42753094+andersonlairton@users.noreply.github.com>
2021-09-25 21:19:07 -0300

    config apontando para o container

--- www/integrado/config.php
+++ www/integrado/config.php
@@ -1,8 +1,13 @@
 <?php
  
-    $host = "db4free.net";//"bd";//"127.0.0.1";//futuramente alterar para o 000webhost
-    $user = "site_compras";
-    $pass = "!tB4=R&DdA|]36%*";
+    // $host = "db4free.net";//"bd";//"127.0.0.1";//futuramente alterar para o 000webhost
+    // $user = "site_compras";
+    // $pass = "!tB4=R&DdA|]36%*";
+    // $banco="sitecompras";
+
+    $host = "bd";//"127.0.0.1";//futuramente alterar para o 000webhost
+    $user = "root";
+    $pass = "password";
     $banco="sitecompras";
 
     $conexao = mysqli_connect($host, $user, $pass) or die(mysqli_error($conexao));
