f5f8cd72a9015fc2a7cd05607ad2dc55ab6a0b74
andersonlairton <42753094+andersonlairton@users.noreply.github.com>
2022-06-11 21:58:57 -0300

    #em desenvolvimento  - js da nova tela de login,realizando a ação via ajax

--- www/integrado/Views/Usuarios/js/login.js
+++ www/integrado/Views/Usuarios/js/login.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-    console.log('arquivo login.js')
+    console.log('arquivo login.js')    
+    $("#btn_entrar").click(function () { 
+        console.log('35')
+        $.ajax({
+            type: "POST",
+            url: "../../source/configajax.php",
+            data: {
+                action:'login',
+                component:'UsuarioModel',
+                cnpj:$("#cnpj_cli").val(),
+                password:$("#password_cli").val()
+            },
+            dataType: "JSON",
+            success: function (data) {
+                console.log(data);
+            },error:function(){
+                console.log('error 29')
+            }
+        });
+        
+    });
 });
